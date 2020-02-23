0fca5c43e0e36212e9930819eadb66c85fbf92b8
danielorihuelarodriguez@gmail.com <danielorihuelarodriguez@gmail.com>
2020-02-23 19:06:49 +0100

    feature: add kanban, refactor setup
    I added kanban folder to store all things related with kanban.
    I updated the kanban to take vim into account and this new kanban
    application.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-# Install and configure zsh and oh my zsh
 DIR="${HOME}/dotfiles"
+
+# Install my personal kanban
+KANBAN_FOLDER="kanban"
+KANBAN_COMMAND="kanban"
+PERSONAL_KANBAN="my_personal_kanban"
+
+wget https://github.com/greggigon/my-personal-kanban/blob/master/my-personal-kanban-0.8.0.zip\?raw\=true
+mv "my-personal-kanban-0.8.0.zip?raw=true" "${KANBAN_FOLDER}/my-personal-kanban-0.8.0.zip"
+unzip "${KANBAN_FOLDER}/my-personal-kanban-0.8.0.zip" -d "${KANBAN_FOLDER}"
+chmod +x "${KANBAN_FOLDER}}/${KANBAN_COMMAND}"
+cp "${KANBAN_FOLDER}/${KANBAN_COMMAND}" "/bin/${KANBAN_COMMAND}"
+
+# Install and configure zsh and oh my zsh
 ZSH_FOLDER="zsh"
 ZSHRC=".zshrc"
 
-sudo apt install zsh -y
+apt install zsh -y
 chsh -s $(which zsh)
-sudo apt install wget -y
-sudo apt install git -y
+apt install wget -y
+apt install git -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv .oh-my-zsh ZSH_FOLDER
 
 ln -sf "${DIR}/${ZSH_FOLDER}/${ZSHRC}" "${HOME}/${ZSHRC}"
+
+# Install and configure vim
+VIM_FOLDER="vim"
+VIMRC=".vimrc"
+
+apt install vim -y
+ln -sf "${DIR}/${VIM_FOLDER}/${VIMRC}" "${HOME}/${VIMRC}"
