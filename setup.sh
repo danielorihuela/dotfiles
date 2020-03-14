62873ce36f3e2eea40178bc6903647ba6be6df75
danielorihuelarodriguez@gmail.com <danielorihuelarodriguez@gmail.com>
2020-03-14 21:18:47 +0100

    fix: rm old kanban app from setup

--- setup.sh
+++ setup.sh
@@ -2,17 +2,6 @@
 
 DIR="${HOME}/dotfiles"
 
-# Install my personal kanban
-KANBAN_FOLDER="kanban"
-KANBAN_COMMAND="kanban"
-PERSONAL_KANBAN="my_personal_kanban"
-
-wget https://github.com/greggigon/my-personal-kanban/blob/master/my-personal-kanban-0.8.0.zip\?raw\=true
-mv "my-personal-kanban-0.8.0.zip?raw=true" "${KANBAN_FOLDER}/my-personal-kanban-0.8.0.zip"
-unzip "${KANBAN_FOLDER}/my-personal-kanban-0.8.0.zip" -d "${KANBAN_FOLDER}"
-chmod +x "${KANBAN_FOLDER}}/${KANBAN_COMMAND}"
-cp "${KANBAN_FOLDER}/${KANBAN_COMMAND}" "/bin/${KANBAN_COMMAND}"
-
 # Install and configure zsh and oh my zsh
 ZSH_FOLDER="zsh"
 ZSHRC=".zshrc"
