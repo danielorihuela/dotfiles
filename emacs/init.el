a0e40fd21c3972967d6103bd5ed0d08641d4f77f
dani <danielorihuelarodriguez@gmail.com>
2021-07-16 23:13:44 +0200

    feat: remove data included by emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,19 +1,2 @@
 (package-initialize)
 (org-babel-load-file "~/dotfiles/emacs/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Desktop/Master/MasterThesis/dapp.org" "/home/dani/Desktop/emacs/games.org" "/home/dani/Desktop/emacs/interesting-companies.org" "/home/dani/Desktop/emacs/todo.org")))
- '(package-selected-packages
-   (quote
-    (which-key org-make-toc use-package spacemacs-theme org-roam org-ref monokai-pro-theme moe-theme markdown-mode magit ivy-rich gruvbox-theme gnuplot-mode gnuplot flucui-themes counsel color-theme-sanityinc-tomorrow cider auctex afternoon-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
