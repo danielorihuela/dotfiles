7f923c97b6efaf9698edba014747424dcec6f0c6
dani <danielorihuelarodriguez@gmail.com>
2021-07-26 21:57:33 +0200

    refactor: setup script
    We also:
    
    1. Changed fish config for zsh
    2. Removed and updated packages

--- setup.sh
+++ setup.sh
@@ -5,54 +5,45 @@ silent_install() {
     sudo apt-get install "$1" -y 1>/dev/null
 }
 
-# INSTALL TOOLS 
 silent_install curl
 silent_install wget
 silent_install git
 silent_install flameshot
-silent_install emacs
-# Sqlite3 is required for org-roam
-silent_install sqlite3
-silent_install libsqlite3-dev
-# Required to export org-mode to pdf
-silent_install texlive-latex-extra
-# Required for citations
-silent_install biber
-#Required to install alacritty
-sudo add-apt-repository ppa:aslatter/ppa
 silent_install firefox
+silent_install emacs27
+silent_install texlive-latex-extra  
+sudo add-apt-repository ppa:aslatter/ppa -y 1>/dev/null
+sudo apt-get update 1>/dev/null
 silent_install alacritty
-silent_install fish
-chsh -s $(which fish)
+
+silent_install zsh
+chsh -s $(which zsh)
 
 
-# CONFIGURING TOOLS
 
 show_configure_message() {
     echo "Configuring \`$1\`..."
 }
 
+create_symbolic_link() {
+    ln -sf $DOTFILES/$1 $2
+}
+
 DOTFILES=$PWD
 CONFIG=$HOME/".config"
 
-
-show_configure_message fish
-rm -rf $CONFIG/fish
-ln -sf $DOTFILES/fish $CONFIG/
-
+show_configure_message zsh
+create_symbolic_link zsh/.zshrc $HOME
 
 show_configure_message starship
-ln -sf $DOTFILES/startship/starship.toml $CONFIG
-
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+create_symbolic_link startship/starship.toml $CONFIG
 
 show_configure_message emacs
 mkdir -p $CONFIG/emacs
-ln -sf $DOTFILES/emacs/init.el $HOME/.emacs.d/
-
+create_symbolic_link emacs/init.el $HOME/.emacs.d/
 
 show_configure_message alacritty
-ALACRITTY="alacritty"
-ln -sf $DOTFILES/$ALACRITTY/alacritty.yml $CONFIG/alacritty.yml
-
+create_symbolic_link alacritty/alacritty.yml $CONFIG
 
 echo -e "\n\nConfiguration ended. some changes will not take effect until the next time you log in."
