cb91cdc252d660ba09cefc0ca205a87782347efe
dani <danielorihuelarodriguez@gmail.com>
2026-08-23 21:09:08 +0200

    feat: install virtualbox for ubuntu

--- helpers/bootstrap/linux.sh
+++ helpers/bootstrap/linux.sh
@@ -3,9 +3,11 @@ if ! command -v apt-get >/dev/null 2>&1; then
   exit 0
 fi
 
-echo "Installing native Linux packages (ghostty, brave-browser)..."
+echo "Installing native Linux packages (ghostty, brave-browser, ...)"
 sudo apt-get update
 sudo apt-get install -y curl ghostty
+sudo apt-get install -y virtualbox
+sudo apt-get install -y virtualbox-qt
 
 if ! command -v brave-browser >/dev/null 2>&1; then
   sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
