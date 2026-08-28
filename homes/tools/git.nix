4687a088fd52cc4d591b7e8fa388ad2b8d9a671c
Daniel Orihuela Rodriguez <dorihuela@newrelic.com>
2026-08-28 17:43:22 +0200

    feat: git aliases

--- homes/tools/git.nix
+++ homes/tools/git.nix
@@ -7,5 +7,13 @@
       user.email = "danielorihuelarodriguez@gmail.com";
       github.user = "danielorihuela";
     };
+
+    aliases = {
+      la = "config --get-regexp alias";
+      co = "checkout";
+      cap = "!git commit --amend --no-edit && git push -f";
+      cmp = "!git checkout $(git branch --list main master --format='%(refname:short)' | head -n1) && git pull";
+      reset-hard = "!git reset --hard origin/$(git branch --show-current)";
+    };
   };
 }
